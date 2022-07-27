73f7f2672878d1a9ac38c97f5270a90063b18452
Dayllin Moodley <dayllinmoodley@gmail.com>
2022-07-27 04:40:13 +0200

    Uploading Sandbox app

---
+++ AndroidPHPMySQL_test/app/src/main/java/com/example/androidphpmysql_test/Constants.java
@@ -0,0 +1,9 @@
+package com.example.androidphpmysql_test;
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.1.115/Android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+
+}
